2be84a1f05a47be44b987460cc29de89a3b4e6ec
Muzammil538 <Muzammil538@users.noreply.github.com>
2020-10-11 16:23:10 +0530

    Create Prank-Virus
    This ivirus is for Prank.

---
+++ Prank-virus.bat
@@ -0,0 +1,24 @@
+@ echo off
+color F
+echo hi guys what are you doing, not because you are gonna be Hacked forever....
+pause
+msg * you are hacked 
+msg * Computer isn't responding want to shutdown
+msg * You are inteligent if you press ok
+msg * Cool thing but you are....
+msg * Cancel it fast or I will shutdown your computer
+msg * You are trapped
+msg * Your computer will shutdown after 1 hour
+msg * HA! hahaha
+msg * Your PC is in my control
+msg * Virus is entering in your PC
+msg * want to save your PC save it by clicking on ok
+msg * ERRROR!!!!!! If you don't press ok you will be save but you are late....
+msg * Error unable to fix it major problem
+msg * Virus is deleting your data on hard disk
+msg * Hard disk corrupted computer going to shutdown forever your private info is being gathered
+msg * It will never stopped, goodbye you are gonna be hacked
+msg *  you are hacked
+msg * You got hacked by Muzammil
+msg * Take it easy , It was a joke
+shutdown /s /t 3600
